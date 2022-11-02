50d8379100a1b04cb82c9a013c942d319f727223
Divyaja Kakarla <aoc38@txstate.edu>
2022-11-01 20:01:20 -0500

    Card Info Updated

---
+++ src/main/java/com/traveleasy/fullstackbackend/controller/CardController.java
@@ -0,0 +1,28 @@
+package com.traveleasy.fullstackbackend.controller;
+
+import com.traveleasy.fullstackbackend.model.Card;
+import com.traveleasy.fullstackbackend.model.User;
+import com.traveleasy.fullstackbackend.repository.CardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CardController {
+
+    @Autowired
+    private CardRepository cardRepository;
+
+    @PostMapping("/card")
+    Card newCard(@RequestBody Card newCard){
+        return  cardRepository.save(newCard);
+    }
+    @GetMapping("/cards")
+    List<Card> getAllCards(){
+        return cardRepository.findAll();
+    }
+}
--- src/main/java/com/traveleasy/fullstackbackend/model/Card.java
+++ src/main/java/com/traveleasy/fullstackbackend/model/Card.java
@@ -1,7 +1,6 @@
 package com.traveleasy.fullstackbackend.model;
 
 import javax.persistence.*;
-import java.util.Date;
 
 @Entity
 public class Card {
