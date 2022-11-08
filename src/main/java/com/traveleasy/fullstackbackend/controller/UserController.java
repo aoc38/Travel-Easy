94cf3fe6dac20f93bf6cdfd96d81179af8d964ab
Divyaja Kakarla <aoc38@txstate.edu>
2022-11-07 22:45:53 -0600

    saving Card info

--- src/main/java/com/traveleasy/fullstackbackend/controller/UserController.java
+++ src/main/java/com/traveleasy/fullstackbackend/controller/UserController.java
@@ -24,12 +24,14 @@ public class UserController {
     User newUser(@RequestBody User newUser){
         User userData = userRepository.save(newUser);
         newUser.toString();
-        Card userCardInfo = newUser.getCards().get(0);
-        userCardInfo.setCardType(CardType.VISA);
-        Card cardData = new Card(userCardInfo.getCardNumber(),userCardInfo.getExpiryDate(),userCardInfo.getCvv(),
-                userCardInfo.getCardType(),userCardInfo.getCardOwnerName(),true,userData);
-        cardRepository.save(cardData);
-        System.out.println(cardData);
+        if (newUser.getCards() != null) {
+            Card userCardInfo = newUser.getCards().get(0);
+            userCardInfo.setCardType(CardType.VISA);
+            Card cardData = new Card(userCardInfo.getCardNumber(),userCardInfo.getExpiryDate(),userCardInfo.getCvv(),
+                    userCardInfo.getCardType(),userCardInfo.getCardOwnerName(),true,userData);
+            cardRepository.save(cardData);
+            System.out.println(cardData);
+        }
         return userData;
     }
     @GetMapping("/users")
@@ -42,4 +44,24 @@ public class UserController {
         return userRepository.findById(id)
                 .orElseThrow(()->new UserNotFoundException(id));
     }
+
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id){
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setEmail(newUser.getEmail());
+                    user.setFirstName(newUser.getFirstName());
+                    return userRepository.save(user);
+                }).orElseThrow(()->new UserNotFoundException(id));
+    }
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable long id){
+        if(!userRepository.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return "User with "+id+" has been deleted successfully";
+    }
+
 }
