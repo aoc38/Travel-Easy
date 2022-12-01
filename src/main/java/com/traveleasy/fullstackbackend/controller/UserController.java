ab4800b9b46a594c72a8758e4a6645cc3252806d
Divyaja Kakarla <aoc38@txstate.edu>
2022-12-01 14:11:05 -0600

    fix register errors

--- src/main/java/com/traveleasy/fullstackbackend/controller/UserController.java
+++ src/main/java/com/traveleasy/fullstackbackend/controller/UserController.java
@@ -19,110 +19,98 @@ import java.util.List;
 @CrossOrigin("http://localhost:3000/")
 public class UserController {
     public static final String USER = "user";
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private CardRepository cardRepository;
 
     @Override
     protected Object clone() throws CloneNotSupportedException {
         return super.clone();
     }
 
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private CardRepository cardRepository;
-
     @PostMapping("/usersignup")
-    User addUser(@RequestBody RegisterUserDto newUser){
+    Object addUser(@RequestBody RegisterUserDto newUser) {
+        User existingUserName = userRepository.findByUsername(newUser.getUsername());
+        User existingEmail = userRepository.findByEmail(newUser.getEmail());
 
+        System.out.println(existingEmail != null ? "Existing user with email" : "");
+        System.out.println(existingUserName != null ? "Existing user with username" : "");
+
+        User createdUserData = new User();
+        if (existingEmail != null && existingUserName != null) {
+            return new NotFoundException("Username and password already exists!. Please choose a new one.", 0L, USER);
+        } else if (existingEmail != null) {
+            return new NotFoundException("EmailID already exists!. Please choose a new email", 0L, USER);
+        } else if (existingUserName != null) {
+           return new NotFoundException("Username already exists!. Please choose a new username", 0L, USER);
+        } else {
             //creating card object from dto
-        System.out.println("card type string name : "+newUser.getCard().getCardType().name());
-            if (newUser.getCard().getCardType().name() == "VISA"){
+            System.out.println("card type string name : " + newUser.getCard().getCardType().name());
+            if (newUser.getCard().getCardType().name() == "VISA") {
                 newUser.getCard().setCardType(CardType.VISA);
-            } else if (newUser.getCard().getCardType().name() == "MASTERCARD"){
+            } else if (newUser.getCard().getCardType().name() == "MASTERCARD") {
                 newUser.getCard().setCardType(CardType.MASTERCARD);
             }
-            Card cardData = Card.builder()
-                    .cardNumber(newUser.getCard().getCardNumber())
-                    .cardType(newUser.getCard().getCardType())
-                    .expiryDate(newUser.getCard().getExpiryDate())
-                    .cvv(newUser.getCard().getCvv())
-                    .cardOwnerName(newUser.getCard().getCardOwnerName())
-                    .isDefault(true)
-                    .build();
+            Card cardData = Card.builder().cardNumber(newUser.getCard().getCardNumber()).cardType(newUser.getCard().getCardType()).expiryDate(newUser.getCard().getExpiryDate()).cvv(newUser.getCard().getCvv()).cardOwnerName(newUser.getCard().getCardOwnerName()).isDefault(true).build();
 
-        //creating a list for card and adding card data from dto
-        List<Card> cardsList = new ArrayList<>();
-        cardsList.add(cardData);
-        //creating usermiles
-        UserMiles userMilesObj = UserMiles.builder()
-                .milesRemaining(0)
-                .milesEarned(0)
-                .milesRedeemed(0)
-                .build();
-        //creating userobject
-        User userData = User.builder()
-                .email(newUser.getEmail())
-                .username(newUser.getUsername())
-                .mailingAddress(newUser.getMailingAddress())
-                .password(newUser.getPassword())
-                .firstName(newUser.getFirstName())
-                .middleName(newUser.getMiddleName())
-                .lastName(newUser.getLastName())
-                .cards(cardsList)
-                .userMiles(userMilesObj)
-                .build();
-        //save user data to DB
-        User createdUserData = userRepository.save(userData);
+            //creating a list for card and adding card data from dto
+            List<Card> cardsList = new ArrayList<>();
+            cardsList.add(cardData);
+            //creating usermiles
+            UserMiles userMilesObj = UserMiles.builder().milesRemaining(0).milesEarned(0).milesRedeemed(0).build();
+            //creating userobject
+            User userData = User.builder().email(newUser.getEmail()).username(newUser.getUsername()).mailingAddress(newUser.getMailingAddress()).password(newUser.getPassword()).firstName(newUser.getFirstName()).middleName(newUser.getMiddleName()).lastName(newUser.getLastName()).cards(cardsList).userMiles(userMilesObj).build();
+            //save user data to DB
+            createdUserData = userRepository.save(userData);
 
-        return createdUserData;
+        } return createdUserData;
     }
+
     @GetMapping("/users")
-    List<User> getAllUsers(){
+    List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
     @GetMapping("/user/{id}")
-    User getUserById(@PathVariable Long id){
-        return userRepository.findById(id)
-                .orElseThrow(()->new NotFoundException("",id,USER));
+    User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new NotFoundException("", id, USER));
     }
 
     @PostMapping("/userlogin")
-    Object loginCheck(@RequestBody LoginUserDto loginUserDto){
-    //check user is present for that login
-        User user = userRepository.findByUsernameAndPassword(loginUserDto.getUsername(),loginUserDto.getPassword());
+    Object loginCheck(@RequestBody LoginUserDto loginUserDto) {
+        //check user is present for that login
+        User user = userRepository.findByUsernameAndPassword(loginUserDto.getUsername(), loginUserDto.getPassword());
         User checkUserName = userRepository.findByUsername(loginUserDto.getUsername());
-        if (user == null){
-            if(checkUserName == null){
-                return new NotFoundException("Please check the username entered ",0L,USER);
-            } else if(checkUserName !=null
-                    && (loginUserDto.getPassword() !=null)
-                    && !checkUserName.getPassword().equals(loginUserDto.getPassword())
-            ){
-                return new NotFoundException("Please check the password entered for username " +checkUserName.getUsername(),checkUserName.getId(),USER);
+        if (user == null) {
+            if (checkUserName == null) {
+                return new NotFoundException("Please check the username entered ", 0L, USER);
+            } else if (checkUserName != null && (loginUserDto.getPassword() != null) && !checkUserName.getPassword().equals(loginUserDto.getPassword())) {
+                return new NotFoundException("Please check the password entered for username " + checkUserName.getUsername(), checkUserName.getId(), USER);
             } else {
-                return new NotFoundException("User is not registered",0L,USER);
+                return new NotFoundException("User is not registered", 0L, USER);
             }
         }
-        return  user;
+        return user;
     }
 
     @PutMapping("/user/{id}")
-    User updateUser(@RequestBody User newUser, @PathVariable Long id){
-        return userRepository.findById(id)
-                .map(user -> {
-                    user.setUsername(newUser.getUsername());
-                    user.setEmail(newUser.getEmail());
-                    user.setFirstName(newUser.getFirstName());
-                    return userRepository.save(user);
-                }).orElseThrow(()->new NotFoundException("",id,USER));
+    User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userRepository.findById(id).map(user -> {
+            user.setUsername(newUser.getUsername());
+            user.setEmail(newUser.getEmail());
+            user.setFirstName(newUser.getFirstName());
+            return userRepository.save(user);
+        }).orElseThrow(() -> new NotFoundException("", id, USER));
     }
+
     @DeleteMapping("/user/{id}")
-    String deleteUser(@PathVariable long id){
-        if(!userRepository.existsById(id)){
-            throw new NotFoundException("",id,USER);
+    String deleteUser(@PathVariable long id) {
+        if (!userRepository.existsById(id)) {
+            throw new NotFoundException("", id, USER);
         }
         userRepository.deleteById(id);
-        return "User with "+id+" has been deleted successfully";
+        return "User with " + id + " has been deleted successfully";
     }
 
 }
